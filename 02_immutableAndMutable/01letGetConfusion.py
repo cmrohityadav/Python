e31ca9a4db0a1b61ff8df016ef63f7a14d12ee1e
cmrohityadav <cmrohityadav23@gmail.com>
2024-03-10 22:08:17 +0530

    mutable & Immutable Data Types

---
+++ 02_immutableAndMutable/01letGetConfusion.py
@@ -0,0 +1,14 @@
+username="rohit";
+print(username);
+username="yadav";
+print(username);
+
+number=10;
+print("number : ",number);
+number2=number;
+print("number2 : ",number2);
+
+number=15;
+print("number : ",number);
+print("number2 : ",number2);
+
---
+++ 02_immutableAndMutable/readme.md
@@ -0,0 +1,15 @@
+# mutable Data Types
+jo change ho skta hai
+* List
+* Set
+* Dictionary
+* Bytearray
+* Array
+# Immutable Data Types
+* Integers
+* Floating -point Number
+* Boolean
+* String
+* Tuples
+* Frozen Set
+* Bytes
