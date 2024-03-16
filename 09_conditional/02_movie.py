22076e06e1609659fc6c667f5dde2efed3e57495
cmrohityadav <cmrohityadav23@gmail.com>
2024-03-16 14:17:45 +0530

    movie my soln

---
+++ 09_conditional/02_movie.py
@@ -0,0 +1,14 @@
+age=int(input("Enter age :"))
+day=input("Enter the 3 letter of the day :")
+
+
+if day=="wed":
+    if(age<18):
+        print(8-2)
+    else:
+        print(12-2)
+else:
+    if(age<18):
+        print(8)
+    else:
+        print(12)
