62f93d288f230b2a32a73033214ce0e3053cbc56
cmrohityadav <cmrohityadav23@gmail.com>
2024-03-16 14:07:09 +0530

    age soln

---
+++ 09_conditional/01_ageGroup.py
@@ -0,0 +1,11 @@
+userAge=int(input("enter age :"))
+
+if userAge<13:
+    print("Child")
+elif userAge<20:
+    print("Teen ager")
+elif userAge<60:
+    print("adult")
+else:
+    print("Senior")
+
