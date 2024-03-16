556864fa85a54600e37b78081e57ac25c3f5676d
cmrohityadav <cmrohityadav23@gmail.com>
2024-03-16 14:07:00 +0530

    Tuple

---
+++ 08_Tuples/01_.py
@@ -0,0 +1,14 @@
+tea_types=("black","green","Oolong")
+print(tea_types)
+print(tea_types[0])
+print(tea_types[-1])
+
+# tea_types[0]="rohit"
+#error , immutable
+
+more_tea=("herbal","earl greay")
+
+all_tea=tea_types+more_tea
+
+print(all_tea)
+
