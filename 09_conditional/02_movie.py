6b844f5ad3306028a7a26bba5c1bee5497d1566c
cmrohityadav <cmrohityadav23@gmail.com>
2024-03-16 14:24:50 +0530

    improve

--- 09_conditional/02_movie.py
+++ 09_conditional/02_movie.py
@@ -2,13 +2,7 @@ age=int(input("Enter age :"))
 day=input("Enter the 3 letter of the day :")
 
 
+price=12 if age>=18 else 8
 if day=="wed":
-    if(age<18):
-        print(8-2)
-    else:
-        print(12-2)
-else:
-    if(age<18):
-        print(8)
-    else:
-        print(12)
+    price-=2
+print("Ticket price for $",price)
